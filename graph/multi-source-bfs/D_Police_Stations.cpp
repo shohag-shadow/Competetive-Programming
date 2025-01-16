ac8d6ada2b887d21ae1e7e1a49a55dee78180a67
Md Shohag Showdagor <mdshohagshowdagor001@gmail.com>
2025-01-16 15:47:09 +0600

    updated it again

--- graph/multi-source-bfs/D_Police_Stations.cpp
+++ graph/multi-source-bfs/D_Police_Stations.cpp
@@ -23,5 +23,52 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    
+    ll n,k,d;
+    cin>>n>>k>>d;
+    queue<ll>q;
+    vector<ll>dis(n+1,1000000000000);
+    ll parent[n+1];
+    for(int i=0;i<k;i++)
+    {
+        ll t;
+        cin>>t;
+        q.push(t);
+        dis[t]=0;
+        parent[t]=t;
+    }
+    vector<ll>graph[n+1];
+    vector<pair<ll,ll>>edges;
+    for(int i=1;i<n;i++)
+    {
+        ll a,b;
+        cin>>a>>b;
+        graph[a].push_back(b);
+        graph[b].push_back(a);
+        edges.push_back({a,b});
+    }
+    while(q.size())
+    {
+        ll par=q.front();
+        q.pop();
+        for(auto child:graph[par])
+        {
+            if(dis[child]>dis[par]+1)
+            {
+                dis[child]=dis[par]+1;
+                q.push(child);
+                parent[child]=par;
+            }
+        }
+    }
+    vector<ll>ans;
+    for(int i=0;i<edges.size();i++)
+    {
+        ll a=edges[i].first;
+        ll b=edges[i].second;
+        if(a==parent[b]||b==parent[a]);
+        else ans.push_back(i+1);
+    }
+    cout<<ans.size()<<endl;
+    for(auto u:ans)cout<<u<<" ";
+    cout<<endl;
 }
